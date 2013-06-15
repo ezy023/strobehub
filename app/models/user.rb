504aa160a7e5900521c13cd59845a488e43cb48e
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-15 14:38:02 -0500

    changed format of validations for easier readability

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,13 @@
 class  User < ActiveRecord::Base
 
   attr_accessible :username, :email, :password, :password_confirmation
-
-  validates_uniqueness_of :username, :case_sensitive => false
-  validates_uniqueness_of :email, :case_sensitive => false
-
-  validates_presence_of :username, :email, :password
-  validates :email, :presence => true, :uniqueness => true, :format => {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
-
+  
   has_secure_password
 
-  validates_confirmation_of :password
+  validates :username, :presence => true, :uniqueness => true, :case_sensitive => false
+  validates :email, :presence => true, :uniqueness => true, :format => {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}, :case_sensitive => false
+  validates :password, :presence => true, :confirmation => true,
+
 
   has_many :versions
   has_many :repositories, :through => :versions
