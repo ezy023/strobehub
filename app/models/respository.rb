1fccce99a6740a14a8d553dddefada39256e226f
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-15 15:21:00 -0500

    added validations to other models

--- app/models/respository.rb
+++ app/models/respository.rb
@@ -5,5 +5,8 @@ class Repository < ActiveRecord::Base
   has_many :versions
   has_many :user, :through => :versions
   belongs_to :creator, :class_name => "User", :foreign_key => :creator_id
+  
+  validats :name, :description,, :creator_id, :presence => true
+
 
 end
--- app/models/version.rb
+++ app/models/version.rb
@@ -1,7 +1,9 @@
 class Version < ActiveRecord::Base
   
-  has_many :tracks
+  has_many   :tracks
   belongs_to :user
   belongs_to :repository
+
+  validates :user_id, :repository_id, :presence => true  
 
 end
