b2d3a9cec5e2122b58de2176f5c44a6e3323e26e
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-15 15:29:46 -0500

    fixed typos in repository model

--- app/models/respository.rb
+++ app/models/respository.rb
@@ -6,7 +6,7 @@ class Repository < ActiveRecord::Base
   has_many :user, :through => :versions
   belongs_to :creator, :class_name => "User", :foreign_key => :creator_id
   
-  validats :name, :description,, :creator_id, :presence => true
+  validates :name, :description, :creator_id, :presence => true
 
 
 end
