3b8d7788d355f562c9f346f59deeb3849caa3ed6
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-15 13:12:06 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Strobehub::Application.config.secret_token = 'dd0612dbe342696a350c500bafa17df1d31ac899f2ea6b4639ed60795ad8556a19fec8ab8ff09820690edd4b7d8bbb9bcd55e0eb2dbeca1ee0f652fd91e31cae'
