ec613056d8fa8e80acc7b339184a598d6b873234
generalzhou <jackzhou999@gmail.com>
2013-06-16 12:35:39 -0500

    added a sweet font

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ Strobehub::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+  # Precompile additional assets
+  # config.assets.precompile += %w( .svg .eot .woff .ttf )
 end
