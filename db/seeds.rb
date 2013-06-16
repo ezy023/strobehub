f519cd0df2b369a2675be90ab8a1d32a5c219f02
Lora <lora.abe@gmail.com>
2013-06-15 20:07:07 -0500

    Fixed dupe name/desc for Repos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,10 @@ require 'faker'
 end
 
 Repository.create(:name => "Space Jam Crunk Rum", :description => "These beats will make you strobe your face off", :creator_id => 1)
-Repository.create(:name => "Space Jam Crunk Rum", :description => "These beats will make you strobe your face off", :creator_id => 2)
+Repository.create(:name => "Face Slam Rage Crush", :description => "rage rage rage rage", :creator_id => 2)
 
 4.times do
 	Version.create(:user_id => User.all.sample.id, :repository_id => Repository.all.sample.id)
 end
+
 
