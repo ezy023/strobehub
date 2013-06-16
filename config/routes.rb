bc968d40db29f1b6fde2d21e25566c6346a5864f
dhouk24 <dhouk24@gmail.com>
2013-06-16 16:00:22 -0500

    signup and login forms and views added

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Strobehub::Application.routes.draw do
   end
 
   get   '/'       => 'static_pages#index'
-  get   '/login'  => 'static_pages#login'
+  get   '/login'  => 'static_pages#new'
   post  '/login'  => 'static_pages#login'
   get   '/logout' => 'static_pages#logout'
 
