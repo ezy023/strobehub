95712bcfa90cf4ffbed0809474cc239809438403
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-17 13:37:39 -0500

    Wrapped entry in closure to fix multiple file async upload

--- app/assets/javascripts/drop.js
+++ app/assets/javascripts/drop.js
@@ -1,13 +1,8 @@
 window.addEventListener('load', function(){
   var dropzone = document.getElementById('dropzone');
-  // var header = document.getElementById('paragraph');
-
-  // header.ondragstart = function(e){
-  //   console.log("Drag has started"); 
-  // }
   dropzone.ondragover = function(e){
     // var data = e.dataTransfer.mozSetData("application/x-moz-file", file, 0);
-    e.preventDefault(); 
+    e.preventDefault();
     var dt = e.dataTransfer;
   }
 
@@ -15,34 +10,55 @@ window.addEventListener('load', function(){
     e.preventDefault();
     var dt = e.dataTransfer;
     var files = dt.files;
-    var xhr = new XMLHttpRequest();
     var form = document.getElementById("song_upload");
     var formData = new FormData(form);
     var entry;
+
+    console.log(files.length);
     for (var i = 0; i < files.length; i++) {
+      var xhr = new XMLHttpRequest();
       entry = files[i];
-      //if(entry instanceof File){ console.log("ITS A FILE"); console.log(entry.name);  } 
+      console.log(entry.name);
+      //if(entry instanceof File){ console.log("ITS A FILE"); console.log(entry.name);  }
       formData.append("song_file", entry);
       xhr.open("POST", "http://localhost:3000/tracks", false);
       xhr.onload = function(evt){
-        var songs = document.getElementById('songs');
-        var song_div = document.createElement('audio');
-        var source_div = document.createElement('source');
-        source_div.setAttribute('src', xhr.response);
-        source_div.setAttribute('type', "audio/mp3");
-        song_div.setAttribute('controls', "controls");
-        song_div.innerHTML += "Song";
-        song_div.appendChild(source_div);
-        songs.appendChild(song_div);
-        console.log(song_div);
+        audioClosure(entry, i, xhr)();
       }
       xhr.send(formData);
     }
-    console.log("THE DROP");
   }
 
+  function audioClosure(entry, i, xhr){
+    var entry = entry;
+    var iteration = i;
+    return function(){
+      var songs = document.getElementById('songs');
+      var song_div = document.createElement('audio');
+      var trackName = document.createTextNode(entry.name);
+      var track_div = document.createElement('div');
+      track_div.appendChild(trackName);
+      track_div.setAttribute('id', "track_" + i);
+      var source_div = document.createElement('source');
+      source_div.setAttribute('src', xhr.response);
+      source_div.setAttribute('type', "audio/mp3");
+      song_div.setAttribute('controls', "controls");
+      song_div.innerHTML += "Song";
+      song_div.appendChild(source_div);
+      songs.appendChild(track_div);
+      songs.appendChild(song_div);
+    }
+  }
+
+ //function progressDiv(xhrObject, trackDiv){
+ //   xhrObject.upload.addEventListener('progress', function(evt){
+ //     var pc = parseInt(100 - (evt.loaded/evt.total * 100);
+ //     console.log(evt.loaded/evt.total*100);
+ //   });
+ //}
+
   var uploadFile = function(file){
-    var xhr = new XMLHttpRequest(); 
+    var xhr = new XMLHttpRequest();
   }
 
 });
