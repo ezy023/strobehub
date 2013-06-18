913e239bce1d4c8fbfb186da3ebe1a19a2540a6c
dhouk24 <dhouk24@gmail.com>
2013-06-18 09:42:01 -0500

    removed extra comment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,6 @@
 	def show
 		@user = User.find(params[:id])
 		@user_repos = @user.created_repositories
-		# @user_repos = sRepository.where(:creator_id => params[:id])
 		@forked_repos = @user.versions.reject {|version| version.repository.creator == @user}.map {|version| version.repository}
 	end
 
