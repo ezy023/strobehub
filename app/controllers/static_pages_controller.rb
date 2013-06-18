f0d0db24a52577634ce3ff81c5eb96b2d51f21ed
generalzhou <jackzhou999@gmail.com>
2013-06-17 23:02:01 -0500

    made some stylin changes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,13 @@ class StaticPagesController < ApplicationController
 	skip_before_filter :require_login
 
 	def index
-		#home page
+		@repositories = Repository.all.sort_by do |repo|
+			-repo.versions.count
+		end[0..4]
+
+		@users = User.all.sort_by do |user|
+			-user.versions.count
+		end[0..4]
 	end
 
 	def new
@@ -11,7 +17,7 @@ class StaticPagesController < ApplicationController
 	def login
 		user = User.find_by_username(params[:static_pages][:username])
 		if user && user.authenticate(params[:static_pages][:password])
-			session[:user_id] = user.id 
+			session[:user_id] = user.id
 			flash[:success] = "You successfully logged in"
 			redirect_to user_path(user)
 		else
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def get_random_image
+   image_exts = %w( .jpg .gif .png )
+    all_files = Dir.entries('app/assets/images/backgrounds')
+    image_files = all_files.delete_if{|x| !image_exts.include?(x[-4..-1])}
+    "assets/backgrounds/#{image_files.sample}"
+  end
 end
