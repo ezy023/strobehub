65b03f49fd466c18e4a677bbb8db7f8291735b93
Lora <lora.abe@gmail.com>
2013-06-18 19:35:54 -0500

    removed presence validation on master_version_id

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -7,7 +7,7 @@ class Repository < ActiveRecord::Base
   belongs_to :creator, :class_name => "User", :foreign_key => :creator_id
   belongs_to :master_version, :class_name => "Version", :foreign_key => :master_version_id
 
-  validates :name, :description, :creator_id, :master_version_id, :presence => true
+  validates :name, :description, :creator_id, :presence => true
 
   def assign_master(version)
   	self.master_version_id = version.id
