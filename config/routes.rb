235c802c9016871399bd806d13e0c204e184b0ce
Lora <lora.abe@gmail.com>
2013-06-19 09:44:55 -0500

    fixes broken route do/end statement

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,7 @@ Strobehub::Application.routes.draw do
   resources   :tracks
 
   resources   :favorites, :only => :create
-  resources   :repositories do
-
   resources   :repositories, :except => :index do
-
     resources :versions, :except => [:edit, :update, :create]
   end
   resources   :tags, :only => [:index, :show]
