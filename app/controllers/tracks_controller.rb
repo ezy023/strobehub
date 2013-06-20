b99b4e3e34b9a7afdbb8edd699d81f8b17c29ac2
generalzhou <jackzhou999@gmail.com>
2013-06-20 10:33:35 -0500

    Tracks successfully upload to S3 bucket

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,18 +1,13 @@
  class TracksController < ApplicationController
 
 	def create
-    @audio_source = AudioSource.new(:file => params[:song_file])
-    if @audio_source.save
-      flash[:success] = "File Uploaded"
-      @audio_source.file.url
-      new_track = Track.new(:url => @audio_source.file.url)
-      respond_to do |format|
-        format.json { render :json => new_track.url }
+      @audio_source = AudioSource.new(:file => params[:song_file])
+      if @audio_source.save
+        #new_track = Track.new(:url => @audio_source.file.url)
+        render :json => @audio_source.file.url
+      else
+        render :json => "There was an Error"
       end
-    else
-      flash[:error] = "No good"
-      redirect_to :back
-    end
 	end
 
 	def update
