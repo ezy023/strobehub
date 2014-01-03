7b352ac54b0943fbc2c797752191e526045da2db
allareri <allareri@gmail.com>
2014-01-03 11:07:50 -0600

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider                   => 'AWS',
-    :aws_access_key_id          => "AKIAI4PFY5SQJCS2MFRA",
-    :aws_secret_access_key      => "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"
+    :aws_access_key_id          => "",
+    :aws_secret_access_key      => ""
 }
   config.fog_directory = "strobehub"
 end
